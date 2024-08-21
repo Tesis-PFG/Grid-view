1ae4d835a3c2c5d3a4115d16005601a687d72180
juan <juanmanuelduranrueda@gmail.com>
2024-08-21 14:38:40 -0500

    DEVELOPMENT app working without opening the browser, needs to be replaced in PRODUCTION, still exposing port 3000

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const { spawn } = require('child_process');
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        },
-    });
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    win.loadURL('http://localhost:3000');
+  mainWindow.loadURL('http://localhost:3000');
+  // mainWindow.webContents.openDevTools(); // Abre las herramientas de desarrollo de Electron
 }
 
 app.whenReady().then(() => {
-    //const pythonProcess = spawn('python', ['python_scripts/your_python_script.py']);
-    // correr de manera simultanea el proceso de python con un spawn
+  createWindow();
 
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
